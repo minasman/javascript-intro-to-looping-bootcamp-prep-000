0bb538a8c9d24605668bb944919b49ae2906f56d
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 19:29:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(arr){
   for(var i=0; i<25; i++){
     if( i === 1){
-      arr[i] = 'I am 1 strange loop.';
+      arr.push('I am 1 strange loop.');
     } else {
-      arr[i] = `I am ${i} strange loops.`;
+      arr.push(`I am ${i} strange loops.`);
     }
   }
   return arr;
